3fbb957bc8191e0447af0db5c52ccfb4bfb70b79
Cemre Can Kaya <cemre_cankaya@hotmail.com>
2021-03-21 20:28:19 +0300

    Operation-Four

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OperationFourComponent } from './components/operation-four/operation-four.component';
 import { OperationOneComponent } from './components/operation-one/operation-one.component';
 import { OperationThreeComponent } from './components/operation-three/operation-three.component';
 import { OperationTwoComponent } from './components/operation-two/operation-two.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path:"operation-one", component:OperationOneComponent},
   {path:"operation-two", component:OperationTwoComponent},
   {path:"operation-three", component:OperationThreeComponent},
+  {path:"operation-four", component:OperationFourComponent},
 
 ];
 
--- src/app/components/operation-four/operation-four.component.ts
+++ src/app/components/operation-four/operation-four.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./operation-four.component.css']
 })
 export class OperationFourComponent implements OnInit {
-
   constructor() { }
 
+  urlList = new Array();
+
+  addUrl(text:string){
+    var control = false;
+    if(text != " " && text!= ""){
+        this.urlList.forEach(p=> {if(p === text) control=true;});
+        if(!control)
+          this.urlList.push(text); 
+    }        
+  }
+  removeUrl(index:number){
+    this.urlList.splice(index,1);
+  }
   ngOnInit(): void {
   }
-
 }
